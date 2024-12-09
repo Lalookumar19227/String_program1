f17c8161f17ed0af7f279b9c36f83b6955ecf56f
Lalookumar19227 <imykl27@gmail.com>
2024-12-09 13:14:24 +0530

    first commit

---
+++ string_program/Program1.java
@@ -0,0 +1,11 @@
+package string_program;
+
+public class Program1 {
+	public static void main(String[] args) {
+		String s1="mohan"+'\0';
+		
+		System.out.println(s1.charAt(3));
+		System.out.println(s1.charAt(5));
+	}
+
+}
